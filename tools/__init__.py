9c07706133e9841dd5979c9b79f951c04f76b3d0
Avishake Adhikary <avishake-adhikary@miniontek.com>
2025-09-10 16:12:08 +0530

    Added Disease Understanding

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,9 +1,12 @@
+from tools.disease_search import search_diseases_schema
 from .get_time import get_time, get_time_schema
 from .calculate import calculate, calculate_schema
 from .search_web import search_web, search_web_schema
 from .get_weather import get_weather, get_weather_schema
 from .save_patient_data import save_patient_data, save_patient_data_schema
 from .fetch_patient_data import fetch_patient_data, fetch_patient_data_schema
+from .disease_search import search_diseases
+from .save_visit_diseases import get_visit_diseases_schema, save_visit_diseases, get_visit_diseases, save_visit_diseases_schema
 
 # Dictionary mapping function names to actual functions
 TOOL_FUNCTIONS = {
@@ -13,6 +16,8 @@ TOOL_FUNCTIONS = {
     "get_weather": get_weather,
     "save_patient_data": save_patient_data,
     "fetch_patient_data": fetch_patient_data,
+    "search_diseases": search_diseases,
+    "save_visit_diseases": save_visit_diseases,
 }
 
 # List of tool schemas for OpenAI function calling
@@ -23,4 +28,7 @@ TOOL_SCHEMAS = [
     get_weather_schema(),
     save_patient_data_schema(),
     fetch_patient_data_schema(),
+    search_diseases_schema(),
+    save_visit_diseases_schema(),
+    get_visit_diseases_schema(),
 ]
